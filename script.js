8db7e5d546166aba3ddad2b7449cc3b300b9c503
Vishakha02D2 <dandagevishakha@gmail.com>
2023-04-15 14:24:46 +0530

    1st commit

--- script.js
+++ script.js
@@ -7,21 +7,31 @@ let arr = [
 ];
 
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  arr.map((item)=>console.log(item))
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(console.log)
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id: 4, name: "karen", age: "19", profession: "admin"})
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter(obj => obj.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let concatenatedArr = arr[0];
+  for(let i = 1; i < arr.length; i++){
+      concatenatedArr = Object.assign(concatenatedArr, arr[i]);
+  }
+  console.log(concatenatedArr)
 }
